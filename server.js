94f3abd80cd3f86b2140f8164f7f1ba65b44a10a
Jonathan Morales <jonmorazav@yahoo.com>
2019-04-27 10:01:35 -0400

    add webpack/babel

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express"),
     app = express(),
+    path = require('path'),
     PORT = process.env.PORT || 3000;
 
-app.disable('x-powered-by')
+app.use(express.static(path.join(__dirname, 'dist')))
+    .use((req, res) => res.sendFile(__dirname + '/dist/index.html'))
+    .disable('x-powered-by')
     .listen(PORT, function() {
         console.log(`Server running on port ${PORT}!`);
     });
