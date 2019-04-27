29fd2bc2cafb4ff9c024d12eb11b00ca7c733a45
Frank Hernandez <fhern077@fiu.edu>
2019-04-27 11:11:29 -0400

    added socket server

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const helmet = require('helmet');
 const mongoose = require("mongoose");
 
+const compression = require('compression');
+
 const app = express();
 const routes = require("./routes");
 const PORT = process.env.PORT || 3000;
@@ -15,11 +17,15 @@ app.use(express.json({ limit: '300kb' }));
 app.use(express.urlencoded({ extended: true }));
 // set up static folder
 app.use(express.static(path.join(__dirname, 'dist')))
+// compress request to improve performance when displaying to clients
+app.use(compression());
+
 
 // use our crud routes
-app.use(routes)
+app.use(routes);
+
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/emerge");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/emerge" , {useNewUrlParser: true} );
 
 module.exports = app;
