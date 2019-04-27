fd47a1b5f2377af8880395eaf5d0030832e4da51
Frank Hernandez <fhern077@fiu.edu>
2019-04-27 15:36:51 -0400

    added axios calls

---
+++ client/src/utils/API.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default {
+  getUserById: function(id) {
+    return axios.get(`http://localhost:$4001/user/${id}`);
+  },
+  getUsers: function() {
+    return axios.get(`http://localhost:$4001/users/`);
+  },
+  getEvents: function() {
+    return axios.get(`http://localhost:$4001/events/`);
+  },
+  getEventsById: function(id) {
+    return axios.get(`http://localhost:$4001/events/${id}`);
+  },
+  createEvents: function(event) {
+    return axios.post(`http://localhost:$4001/events/`, event);
+  }
+};
